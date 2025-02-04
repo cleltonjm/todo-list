39acf3aaaa2493e55ba9015c8b917d095e84685e
Clelton <cleltonjm_95@hotmail.com>
2025-02-03 21:34:36 -0300

    Estilizando botão

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 
 export default function Button() {
     return (
-        <button type="button">
+        <button className={styles.button} type="button">
             Criar
             <PlusCircle size={16} />
         </button>
